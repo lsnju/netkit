e21969667be80e9b79dfbfa33ea02f534160dc10
lisong <song.li@witontek.com>
2017-07-28 16:45:33 +0800

    init

--- netkit/src/main/java/ls/demon/netkit/App.java
+++ netkit/src/main/java/ls/demon/netkit/App.java
@@ -4,10 +4,8 @@ package ls.demon.netkit;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
