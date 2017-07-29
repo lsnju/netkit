9ac6863ccfac25965d5daf8e8ba0bea1d6c057ab
lisong <song.li@witontek.com>
2017-07-29 09:45:20 +0800

    update

--- netkit/src/main/java/ls/demon/netkit/App.java
+++ netkit/src/main/java/ls/demon/netkit/App.java
@@ -1,11 +1,27 @@
 package ls.demon.netkit;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  *
  */
 public class App {
+    /**
+    * Logger for this class
+    */
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
     public static void main(String[] args) {
+        if (logger.isDebugEnabled()) {
+            logger.debug("main(String[]) - start"); //$NON-NLS-1$
+        }
+
         System.out.println("Hello World!");
+
+        if (logger.isDebugEnabled()) {
+            logger.debug("main(String[]) - end"); //$NON-NLS-1$
+        }
     }
 }
