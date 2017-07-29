6c3c4fb137e0144bf02cded313f7c77737305298
lisong <song.li@witontek.com>
2017-07-29 22:22:03 +0800

    add

---
+++ netkit/src/main/java/ls/demon/netkit/util/EhJerseyClient.java
@@ -0,0 +1,98 @@
+/**
+ * Witontek.com.
+ * Copyright (c) 2012-2016 All Rights Reserved.
+ */
+package ls.demon.netkit.util;
+
+import java.util.logging.Level;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.Feature;
+
+import org.glassfish.jersey.apache.connector.ApacheConnectorProvider;
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.client.ClientProperties;
+import org.glassfish.jersey.client.JerseyClientBuilder;
+import org.glassfish.jersey.logging.LoggingFeature;
+import org.glassfish.jersey.logging.LoggingFeature.Verbosity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 
+ * @author song.li@witontek.com
+ * @version $Id: JerseyClient.java, v 0.1 2016年6月21日 上午11:14:44 song.li@witontek.com Exp $
+ */
+public class EhJerseyClient {
+
+    /**  */
+    private static final String       COM_WITON_JERSEY_CLIENT = "jersey.CLIENT";
+
+    /**
+    * Logger for this class
+    */
+    private static final Logger       logger                  = LoggerFactory
+        .getLogger(COM_WITON_JERSEY_CLIENT);
+
+    private static final ClientConfig CLIENT_CONFIG;
+
+    private static final Client       CLIENT;
+
+    public static final String        PROTOCOL                = "TLS";
+
+    public static ClientConfig newClientConfig() {
+
+        ClientConfig config = new ClientConfig();
+
+        config.property(LoggingFeature.LOGGING_FEATURE_VERBOSITY_CLIENT,
+            LoggingFeature.Verbosity.PAYLOAD_ANY);
+        config.property(LoggingFeature.LOGGING_FEATURE_VERBOSITY_SERVER,
+            LoggingFeature.Verbosity.PAYLOAD_ANY);
+
+        config.property(ClientProperties.CONNECT_TIMEOUT, 5 * 1000);
+        config.property(ClientProperties.READ_TIMEOUT, 10 * 1000);
+
+        Level logLevel = Level.FINE;
+        if (logger.isTraceEnabled() || logger.isDebugEnabled()) {
+            logLevel = Level.INFO;
+        }
+        logger.warn("客户端调试日志级别 {}", logLevel);
+        // 1 
+        java.util.logging.Logger log = java.util.logging.Logger.getLogger(COM_WITON_JERSEY_CLIENT);
+        Feature feature = new LoggingFeature(log, logLevel, Verbosity.PAYLOAD_ANY, null);
+        config.register(feature);
+        return config;
+    }
+
+    static {
+        CLIENT_CONFIG = newClientConfig();
+        CLIENT = ClientBuilder.newClient(CLIENT_CONFIG);
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public static Client getJerseyClient() {
+        return CLIENT;
+    }
+
+    public static Client getXX() {
+
+        ClientConfig config = newClientConfig();
+
+        config.connectorProvider(new ApacheConnectorProvider());
+        config.property(ClientProperties.PROXY_URI, "http://127.0.0.1:8888");
+        //config.property(ClientProperties.PROXY_USERNAME,user);
+        //config.property(ClientProperties.PROXY_PASSWORD,pass);
+        Client client = JerseyClientBuilder.newClient(config);
+
+        logger.info("{}", CLIENT_CONFIG);
+        logger.info("{}", config);
+        logger.info("{}", CLIENT_CONFIG.getConnectorProvider());
+        logger.info("{}", config.getConnectorProvider());
+        return client;
+    }
+
+}
---
+++ netkit/src/test/java/ls/demon/netkit/client/ProxyClientTest.java
@@ -0,0 +1,139 @@
+/**
+ * Witontek.com.
+ * Copyright (c) 2012-2017 All Rights Reserved.
+ */
+package ls.demon.netkit.client;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.WebTarget;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ls.demon.netkit.util.EhJerseyClient;
+import ls.demon.netkit.utils.JdkProxyUtils;
+
+/**
+ * 
+ * @author lisong
+ * @version $Id: Client.java, v 0.1 2017年7月29日 上午10:45:40 lisong Exp $
+ */
+public class ProxyClientTest {
+    /**
+    * Logger for this class
+    */
+    private static final Logger logger = LoggerFactory.getLogger(ProxyClientTest.class);
+
+    private String              host   = "127.0.0.1";
+    private String              port   = "8888";
+
+    @Test
+    public void test_http_proxy() {
+        logger.info("");
+        JdkProxyUtils.httpProxy(host, port);
+
+        try {
+            // Next connection will be through proxy.
+            URL url = new URL("http://cn.bing.com/");
+            InputStream in = url.openStream();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            for (;;) {
+                String line = br.readLine();
+                if (line == null) {
+                    break;
+                }
+                logger.info("< {}", line);
+            }
+
+            IOUtils.closeQuietly(in);
+        } catch (Exception e) {
+            logger.error("", e);
+        }
+
+    }
+
+    @Test
+    public void test_https_proxy() {
+        logger.info("");
+        JdkProxyUtils.httpsProxy(host, port);
+
+        try {
+            // Next connection will be through proxy.
+            URL url = new URL("https://www.alipay.com/");
+            InputStream in = url.openStream();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            for (;;) {
+                String line = br.readLine();
+                if (line == null) {
+                    break;
+                }
+                logger.info("< {}", line);
+            }
+
+            IOUtils.closeQuietly(in);
+        } catch (Exception e) {
+            logger.error("", e);
+        }
+
+    }
+
+    @Test
+    public void test_jersy_client_http_proxy_xx() {
+        logger.info("");
+        try {
+            Client client = EhJerseyClient.getXX();
+            WebTarget wt = client.target("http://cn.bing.com/");
+            String resp = wt.request().get(String.class);
+            logger.info("{}", resp);
+        } catch (Exception e) {
+            logger.error("", e);
+        }
+
+    }
+
+    @Test
+    public void test_jersy_client_http_proxy() {
+        logger.info("");
+        //        JdkProxyUtils.httpProxy(host, port);
+        //        JdkProxyUtils.httpsProxy(host, port);
+        JdkProxyUtils.socksProxy(host, port);
+
+        try {
+            Client client = EhJerseyClient.getJerseyClient();
+            WebTarget wt = client.target("http://cn.bing.com/");
+            String resp = wt.request().get(String.class);
+            logger.info("{}", resp);
+        } catch (Exception e) {
+            logger.error("", e);
+        }
+
+    }
+
+    @Test
+    public void test_jersy_client_https_proxy() {
+        logger.info("");
+        //        JdkProxyUtils.httpProxy(host, port);
+        //        JdkProxyUtils.httpsProxy(host, port);
+        JdkProxyUtils.socksProxy(host, port);
+
+        try {
+            Client client = EhJerseyClient.getJerseyClient();
+            WebTarget wt = client.target("https://www.alipay.com/");
+            String resp = wt.request().get(String.class);
+            logger.info("{}", resp);
+        } catch (Exception e) {
+            logger.error("", e);
+        }
+
+    }
+
+}
